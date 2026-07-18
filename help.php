071061a442eea1b593c20dd4d7fb38a96764f66c
Matias David Schwerdt <schwerdt.matias@gmail.com>
2026-07-18 20:39:59 -0300

    Improve help

--- help.php
+++ help.php
@@ -31,36 +31,6 @@ include "Templates/html.tpl";
 							<div class="contentContainer">
 								<div id="content" class="universal"><h1 class="titleInHeader"><?php echo $lang['HELP1']['TITRE']; ?></h1>
 
-<div class="helpInfoBlock">
-	<a target="_blank" href="http://t4.answers.travian.fr/" class="helpHeadLine"><?php echo $lang['HELP1']['Partie_1']; ?></a>
-	<a target="_blank" href="http://t4.answers.travian.fr/" class="helpText"><?php echo $lang['HELP1']['Text_1']; ?></a>
-</div>
-
-<div class="helpInfoBlock">
-	<a target="_blank" href="http://www.travian.fr/spielregeln.php" class="helpHeadLine"><?php echo $lang['HELP1']['Partie_2']; ?></a>
-	<a target="_blank" href="http://www.travian.fr/spielregeln.php" class="helpText"><?php echo $lang['HELP1']['Text_2']; ?></a>
-</div>
-
-<div class="helpInfoBlock">
-	<a href="help.php?page=support" class="helpHeadLine"><?php echo $lang['HELP1']['Partie_3']; ?></a>
-	<a href="help.php?page=support" class="helpText"><?php echo $lang['HELP1']['Text_3']; ?></a>
-</div>
-
-<div class="helpInfoBlock">
-	<a href="plus.php?id=8" class="helpHeadLine"><?php echo $lang['HELP1']['Partie_4']; ?></a>
-	<a href="plus.php?id=8" class="helpText"><?php echo $lang['HELP1']['Text_4']; ?></a>
-</div>
-
-<div class="helpInfoBlock">
-	<a target="_blank" href="http://forum.travian.fr" class="helpHeadLine"><?php echo $lang['HELP1']['Partie_5']; ?></a>
-	<a target="_blank" href="http://forum.travian.fr" class="helpText"><?php echo $lang['HELP1']['Text_5']; ?></a>
-</div>
-
-<div class="helpInfoBlock">
-	<a href="#" class="helpHeadLine" onclick="return Travian.Game.iPopup(0,0);"><?php echo $lang['HELP1']['Partie_6']; ?></a>
-	<a href="#" onclick="return Travian.Game.iPopup(0,0);" class="helpText"><?php echo $lang['HELP1']['Text_6']; ?></a>
-</div>
-
 <div class="helpInfoBlock helpInfoLinkLess">
 	<div class="helpHeadLine"><?php echo $lang['HELP1']['Partie_7']; ?></div>
 	<div class="helpText"><?php echo $lang['HELP1']['Text_7']; ?></div>
