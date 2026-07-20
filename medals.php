cea0e90df15ec08f91fc88972ea08fef7203961e
Matias David Schwerdt <schwerdt.matias@gmail.com>
2026-07-19 23:55:49 -0300

    Fix stats

--- medals.php
+++ medals.php
@@ -82,17 +82,18 @@ include "Templates/html.tpl";
 							Próxima semana a repartir: <b><?php echo $week; ?></b> (jugadores) / <b><?php echo $allyweek; ?></b> (alianzas).
 							<div class="clear"></div>
 						</div>
-						<div class="contentFooter">&nbsp;</div>
 					</div>
-					<?php
-					include("Templates/sideinfo.tpl");
-					include("Templates/footer.tpl");
-					include("Templates/header.tpl");
-					include("Templates/res.tpl");
-					include("Templates/vname.tpl");
-					include("Templates/quest.tpl");
-					?>
+					<div class="contentFooter">&nbsp;</div>
 				</div>
+
+				<?php
+				include("Templates/sideinfo.tpl");
+				include("Templates/footer.tpl");
+				include("Templates/header.tpl");
+				include("Templates/res.tpl");
+				include("Templates/vname.tpl");
+				include("Templates/quest.tpl");
+				?>
 			</div>
 		</div>
 		<div id="ce"></div>
